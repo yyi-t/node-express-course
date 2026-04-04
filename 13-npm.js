4464b7e2a817693a27e99aa344ee80cb0957166f
yyi-t <yingyi.tan99@gmail.com>
2026-04-04 09:23:40 +0800

    Add more npm related notes and nodemon.

--- 13-npm.js
+++ 13-npm.js
@@ -7,6 +7,10 @@
 // global dependency - use it in any project
 // npm install -g <packageName>
 
+// install as dev dependencies
+// npm i <packageName> --save-dev
+// npm i <packageName> -D
+
 // package.json - manifest file (stores important info about project/package)
 // manual approach (create package.json in the root, create properties etc)
 // npm init (step by step, press enter to skip)
@@ -22,3 +26,15 @@ const _ = require('lodash')
 const items = [1, [2, [3, [4]]]]
 const newItems = _.flattenDeep(items)
 console.log(newItems)
+
+// to allow others to install the dependencies
+// npm install
+
+// uninstall package
+// npm uninstall <packageName>
+
+// nuclear approach, manually remove unwanted dependency then remove node_module + lock file
+// then run npm install again to get the lock file n node module
+
+// to know more about package.json --> nodesource.com 's the basics of package.json
+// https://nodesource.com/blog/the-basics-of-package-json
