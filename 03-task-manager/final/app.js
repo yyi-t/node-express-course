ed7f6d12c5fcd4956268300aca53d3c858962781
yyi-t <yingyi.tan99@gmail.com>
2026-04-08 12:42:27 +0800

    [task manager] set up basic tasks api, route and controller.

--- 03-task-manager/final/app.js
+++ 03-task-manager/final/app.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express()
+const tasks = require('./routes/tasks')
+
+// middleware
+app.use(express.json())
+
 
 
 // routes
 app.get('/hello', (req,res) =>{
     res.send("Task manager app")
 })
+
+app.use('/api/v1/tasks', tasks)
 
 
 
