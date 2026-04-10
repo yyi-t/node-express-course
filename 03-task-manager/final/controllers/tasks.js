af1cef041e6b737c7e2b2c86fe76ba2b77da90ec
yyi-t <yingyi.tan99@gmail.com>
2026-04-10 15:47:47 +0800

    [task manager] use find to get all task for api.

--- 03-task-manager/final/controllers/tasks.js
+++ 03-task-manager/final/controllers/tasks.js
@@ -1,11 +1,15 @@
 const Task = require('../models/Task')
 
-const getAllTasks = (req, res) => {
-  res.send('get all task')
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({})
+    res.status(200).json({ tasks })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
 const createTask = async (req, res) => {
-  // res.send('create task')
   try {
     const task = await Task.create(req.body)
     res.status(201).json({ task })
