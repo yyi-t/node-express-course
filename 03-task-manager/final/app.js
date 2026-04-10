9fab6176c94b30a9e4ec19e11f3b624887089c29
yyi-t <yingyi.tan99@gmail.com>
2026-04-10 17:17:26 +0800

    [task manager] Add general error handler middlware.

--- 03-task-manager/final/app.js
+++ 03-task-manager/final/app.js
@@ -4,6 +4,7 @@ const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
 const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
 // middleware
 app.use(express.static('./public'))
@@ -13,6 +14,8 @@ app.use(express.json())
 app.use('/api/v1/tasks', tasks)
 
 app.use(notFound)
+app.use(errorHandlerMiddleware)
+// there's a built in default error handler from express js
 
 const port = 3000
 
