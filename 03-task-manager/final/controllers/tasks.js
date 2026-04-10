525f2a8091c24ac5b880c13d3e3ca1f7e7a6068f
yyi-t <yingyi.tan99@gmail.com>
2026-04-10 15:54:41 +0800

    [task manager] Use findOne to get single task for api.

--- 03-task-manager/final/controllers/tasks.js
+++ 03-task-manager/final/controllers/tasks.js
@@ -18,9 +18,18 @@ const createTask = async (req, res) => {
   }
 }
 
-const getSingleTask = (req, res) => {
-  console.log(req.params.id)
-  res.send('get single task')
+const getSingleTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params
+    const task = await Task.findOne({ _id: taskID })
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` })
+    }
+
+    res.status(200).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
 const updateTask = (req, res) => {
