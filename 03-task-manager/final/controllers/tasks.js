8b8c08a00c315c250c88dc6594254f9641173d91
yyi-t <yingyi.tan99@gmail.com>
2026-04-10 14:32:27 +0800

    [task manager] Add task schema and update create task endpoint with values.

--- 03-task-manager/final/controllers/tasks.js
+++ 03-task-manager/final/controllers/tasks.js
@@ -1,9 +1,13 @@
+const Task = require('../models/Task')
+
 const getAllTasks = (req, res) => {
   res.send('get all task')
 }
 
-const createTask = (req, res) => {
-  res.send('create task')
+const createTask = async (req, res) => {
+  // res.send('create task')
+  const task = await Task.create(req.body)
+  res.status(201).json({ task })
 }
 
 const getSingleTask = (req, res) => {
