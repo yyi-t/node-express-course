267f1953d2e5785983f5c981df6891ea6303eda9
yyi-t <yingyi.tan99@gmail.com>
2026-04-13 20:37:05 +0800

    [store api] add featured, company and name filter for get all products api.

--- 04-store-api/final/controllers/products.js
+++ 04-store-api/final/controllers/products.js
@@ -1,9 +1,26 @@
+const Product = require('../models/product')
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ masg: 'products testing route' })
+  // throw new Error('testing async')
+  const products = await Product.find({})
+  res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'products route' })
+  const { featured, company, name } = req.query
+  const queryObject = {}
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false
+  }
+  if (company) {
+    queryObject.company = company
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' }
+  }
+  const products = await Product.find(queryObject)
+  res.status(200).json({ products, nbHits: products.length })
 }
 
 module.exports = {
