7492a2048e5e4f0ebae9f707829a0dd7832b9888
yyi-t <yingyi.tan99@gmail.com>
2026-04-14 11:22:02 +0800

    [JWT Basic] add jwt sign and throw error if invalid field in login endpoint.

--- 05-JWT-Basics/app.js
+++ 05-JWT-Basics/app.js
@@ -13,9 +13,7 @@ const mainRouter = require('./routes/main')
 app.use(express.static('./public'))
 app.use(express.json())
 
-
 app.use('/api/v1', mainRouter)
-
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
--- 05-JWT-Basics/controllers/main.js
+++ 05-JWT-Basics/controllers/main.js
@@ -1,13 +1,27 @@
-// check username, password 
+// check username, password
 // if exist, create new JWT
 // send it back to front-end
-
 // setup authentication so only the request with JWT can access the dashboard
-
-
+const jwt = require('jsonwebtoken')
+const CustomAPIError = require('../errors/custom-error')
 
 const login = async (req, res) => {
-  res.send('Fake Login/Register/Signup Route')
+  const { username, password } = req.body || {}
+
+  if (!username || !password) {
+    // throw new Error('testing async')
+    throw new CustomAPIError('Please provide the username and password', 400)
+  }
+  // just for demo, normally provided by DB
+  const id = new Date().getDate()
+
+  // jwt: try to keep payload small for better experience(less loading)
+  // jwt secret: just for demo, in prod use long, complex and unguessable string
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d'
+  })
+
+  res.status(200).json({ msg: 'user created', token })
 }
 
 const dashboard = async (req, res) => {
