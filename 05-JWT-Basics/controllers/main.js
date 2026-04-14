0ec52a86181a8561a93efeeef6581a454ec2d5e5
yyi-t <yingyi.tan99@gmail.com>
2026-04-14 12:37:47 +0800

    [JWT Basic] use auth middleware.

--- 05-JWT-Basics/controllers/main.js
+++ 05-JWT-Basics/controllers/main.js
@@ -25,23 +25,12 @@ const login = async (req, res) => {
 }
 
 const dashboard = async (req, res) => {
-  const authHeader = req.headers.authorization
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    throw new CustomAPIError('Please provide the auth token', 401)
-  }
-
-  const token = authHeader.split(' ')[1]
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    const luckyNumber = Math.floor(Math.random() * 100)
-    res.status(200).json({
-      msg: `Hello, ${decoded.username}`,
-      secret: `Here is your authorized data, your lucky number is ${luckyNumber}`
-    })
-  } catch (error) {
-    throw new CustomAPIError('Not authorized to access this route', 401)
-  }
+  // user from authmiddleware
+  const luckyNumber = Math.floor(Math.random() * 100)
+  res.status(200).json({
+    msg: `Hello, ${req.user.username}`,
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`
+  })
 }
 
 module.exports = {
