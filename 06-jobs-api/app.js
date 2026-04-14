2a30f2572f331049a005917f72ffc1add794325e
yyi-t <yingyi.tan99@gmail.com>
2026-04-14 17:05:03 +0800

    [jobs api] setup basic endpoint backbone.

--- 06-jobs-api/app.js
+++ 06-jobs-api/app.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
-require('express-async-errors')
 const express = require('express')
 const app = express()
+
+const connectDB = require('./db/connect')
+const authRouter = require('./routes/auth')
+const jobsRouter = require('./routes/jobs')
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found')
@@ -11,9 +14,8 @@ app.use(express.json())
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api')
-})
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', jobsRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
@@ -22,6 +24,7 @@ const port = process.env.PORT || 3000
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URL)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     )
