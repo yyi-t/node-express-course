52809268c6908edfcf0cc1e0e5551c28fe98c05d
yyi-t <yingyi.tan99@gmail.com>
2026-04-15 11:57:18 +0800

    [jobs api] Use async for presave model and add jwt token for register user.

--- 06-jobs-api/models/Users.js
+++ 06-jobs-api/models/Users.js
@@ -24,10 +24,9 @@ const UserSchema = new mongoose.Schema({
   }
 })
 
-UserSchema.pre('save', async function (next) {
+UserSchema.pre('save', async function () {
   const salt = await bcrypt.genSalt(10)
   this.password = await bcrypt.hash(this.passward, salt)
-  next()
 })
 
 module.exports = mongoose.model('User', UserSchema)
