8d115dfda491aa7422ed4733441aca3a0209f419
yyi-t <yingyi.tan99@gmail.com>
2026-04-15 11:20:25 +0800

    [jobs api] use bcrypt to has password in register api.

--- 06-jobs-api/controllers/auth.js
+++ 06-jobs-api/controllers/auth.js
@@ -1,5 +1,15 @@
+const User = require('../models/Users')
+const { StatusCodes } = require('http-status-codes')
+const bcrypt = require('bcryptjs')
+
 const register = async (req, res) => {
-  res.send('register user')
+  const { name, email, password } = req.body
+
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  const user = await User.create({ name, email, password: hashedPassword })
+  res.status(StatusCodes.CREATED).json({ user })
 }
 
 const login = async (req, res) => {
