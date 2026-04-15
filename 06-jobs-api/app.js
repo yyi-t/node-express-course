df9e758b7253c1ca47ff9118b1b1cab64e371201
yyi-t <yingyi.tan99@gmail.com>
2026-04-15 14:22:02 +0800

    [jobs api] add auth middleware to validate user in jobs endpoint.

--- 06-jobs-api/app.js
+++ 06-jobs-api/app.js
@@ -5,6 +5,7 @@ const app = express()
 const connectDB = require('./db/connect')
 const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
+const authenticateUser = require('./middleware/authentication')
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found')
@@ -15,7 +16,7 @@ app.use(express.json())
 
 // routes
 app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/jobs', jobsRouter)
+app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
