bb5026f8a1ff580bd7190a0c02023039e556a0d9
yyi-t <yingyi.tan99@gmail.com>
2026-04-15 11:35:01 +0800

    [jobs api] move hash password logic to presave in model schema.

--- 06-jobs-api/controllers/auth.js
+++ 06-jobs-api/controllers/auth.js
@@ -1,14 +1,8 @@
 const User = require('../models/Users')
 const { StatusCodes } = require('http-status-codes')
-const bcrypt = require('bcryptjs')
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body
-
-  const salt = await bcrypt.genSalt(10)
-  const hashedPassword = await bcrypt.hash(password, salt)
-
-  const user = await User.create({ name, email, password: hashedPassword })
+  const user = await User.create({ ...req.body })
   res.status(StatusCodes.CREATED).json({ user })
 }
 
